9fe3eff41648574f0c9ca0082f15c32709695288
Ravi Soni <ravi.bluepixel@gmail.com>
2022-01-19 13:07:02 +0530

    Added new actvity

---
+++ app/src/main/java/com/test/learngitandgithub2/testpkg/TestingActivity.kt
@@ -0,0 +1,12 @@
+package com.test.learngitandgithub2.testpkg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test.learngitandgithub2.R
+
+class TestingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing)
+    }
+}
